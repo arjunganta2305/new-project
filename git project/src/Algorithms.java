3679991cd9ca0e0c7dfb9738a2229c5ade301201
hp <hp@DESKTOP-V18BIBQ>
2019-06-18 23:20:16 +0530

    2nd commit for git check

--- git project/src/Algorithms.java
+++ git project/src/Algorithms.java
@@ -2,6 +2,7 @@
 public class Algorithms {
 	public static void main(String[] a){
 		System.out.println("hai this is my new code which is to be pushed to git hub");
+		System.out.println("Check git");
 	}
 
 }
